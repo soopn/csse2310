037150268385201e0f74a804b928a56024a76cce
s4873930@uq.edu.au <s4873930@uq.edu.au@21130727-1c1a-8f4d-90a0-7afe4296f637>
2025-03-12 10:05:13 +0000

    Rough version of makefile, added basic expression function using te_interp, made rough layout of how to parse arguments from stdin
    git-svn-id: file:///mnt/c/Users/ivana/local-repo/trunk@2 21130727-1c1a-8f4d-90a0-7afe4296f637

---
+++ a1/uqexpr.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <tinyexpr.h>
+#include <stdlib.h>
+#include <string.h>
+
+bool var_check(char* input){
+    for (int i=0 ; i < strlen(input) ; i++){
+        if (input[i] != '=') {
+            return FALSE;
+        }
+        else {
+            return TRUE;
+        }
+    }
+}
+
+/*
+bool eqn_check (char* input) {
+    for (int i=0 ; i<strlen(input) ; i++) {
+       if (input[i]  
+			   }
+*/
+/*
+int argument_check (char* input) {
+	if (input == "initialise") {
+		initialise();	// incomplete
+	}
+	else if (input == "sigfigures") {
+		sigfigures();	// incomplete
+	}
+	else if (input == "looping") {
+		looping();		// incomplete
+	}
+*/
+
+int argument_check (int argc, char* input) {
+
+	char* arguments[3] = {"initialise" , "looping" , "sigfigures"};
+
+	for (int i = 1 ; i < argc ; i++) {
+		for (int j = 0 ; j < 3 ; j++) {
+			if (strstr(input[i], arguments[j])) {
+					return j;
+			}
+		}
+	}
+}
+
+int main (int argc, char* argv[]) //include some while clause for EOF e.g fgets()
+{
+	char* arguments[3] = {"initialise", "sigfigures", "looping"};
+
+    argc--;
+    char* expression = malloc(sizeof(char**));
+
+    if (argc < 1) {			// if no arguments run normal calc 
+        printf("No loop variables were found\n");
+        scanf("%s", expression);
+    }
+	for (int i = 1 ; i < argc ; i++) {		// find which arguments to run
+		switch(argument_check(argc, argv[i])) {
+				case 0:
+				initialise();
+				break;
+
+				case 1:
+				looping();
+				break;
+
+				case 2:
+				sigfigures();
+				break;
+
+				default: break;
+		}
+	
+
+	
+
+
+    return 0;
+}

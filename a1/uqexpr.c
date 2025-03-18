79fe7f67a8592a1fb1c45277a3d9bed80b4f03ea
s4873930@uq.edu.au <s4873930@uq.edu.au@21130727-1c1a-8f4d-90a0-7afe4296f637>
2025-03-18 02:07:29 +0000

    added debugger flag to makefile, created outline for storing variables and fixed bug while checking for arguments
    git-svn-id: file:///mnt/c/Users/ivana/local-repo/trunk@4 21130727-1c1a-8f4d-90a0-7afe4296f637

--- a1/uqexpr.c
+++ a1/uqexpr.c
@@ -3,6 +3,10 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define TRUE 1
+#define FALSE 0
+
+/*
 bool var_check(char* input){
     for (int i=0 ; i < strlen(input) ; i++){
         if (input[i] != '=') {
@@ -13,6 +17,7 @@ bool var_check(char* input){
         }
     }
 }
+*/
 
 /*
 bool eqn_check (char* input) {
@@ -33,9 +38,7 @@ int argument_check (char* input) {
 	}
 */
 
-int argument_check (int argc, char* input) {
-
-	char* arguments[3] = {"initialise" , "looping" , "sigfigures"};
+int argument_check (int argc, char** input, char* arguments) {
 
 	for (int i = 1 ; i < argc ; i++) {
 		for (int j = 0 ; j < 3 ; j++) {
@@ -44,52 +47,78 @@ int argument_check (int argc, char* input) {
 			}
 		}
 	}
-	return 0; //unsure about this return value maybe greater than 3?
+	return -1; // returning -1 will send it to default case
 }
 
-bool valid_use(int argc, char* argv[]) {
-	for (int i = 1 ; i < argc - 1 ; i++) {
-		if (strstr(argv[i],"--") == FALSE) {
-				return FALSE;
+void valid_use(int argc, char* argv[]) { 
+	for (int i = 1 ; i < argc - 1 ; i+=2) {  // skip over any strings that would follow the arguments
+		if (strstr(argv[i],"--") == FALSE) { // if any file before the arguments exit program
+		fprintf(stderr, "Usage: ./uqexpr [--looping string] [--sigfigures 2..9] [--initialise string] [filename]\n");
+		exit(9); 
 		}
 	}
-	return TRUE;
 }
 
 int main (int argc, char* argv[]) //include some while clause for EOF e.g fgets()
 {
-	char* arguments[3] = {"initialise", "sigfigures", "looping"};
+	char* arguments[3] = {"--initialise", "--sigfigures", "--looping"};
+	int init_count = 0;
+
+	for (int i = 1 ; i < argc - 1; i++) {
+		if (strstr(argv[i], arguments[1])) {
+			init_count++;
+		}
+	}
+	te_variable teVars[init_count];	
+	double values[init_count];
 
     argc--; // argc starts at first argument not at ./uqexpr
     char* expression = malloc(sizeof(char**));
-	
-	if (argc > 1 && valid_use(argc, argv)) { 
-		fprintf(stderr, "Usage: ./uqexpr [--looping string] [--sigfigures 2..9] [--initialise string] [filename]\n");
-		return 9;
-		}
-    if (argc < 1) {			// if no arguments run normal calc 
-        printf("No loop variables were found\n");
-        scanf("%s", expression);
-    }
-	for (int i = 1 ; i < argc ; i++) {		// find which arguments to run
-		switch(argument_check(argc, argv[i])) {
-				case 0:
-				initialise();
+	if (argc > 1 ) { 
+		valid_use(argc, argv);
+		int value_array = 0;
+		for (int i = 1 ; i < argc - 1 ; i++) {		// find which arguments to run (-1 to exclude filename)
+			switch(argument_check(argc, argv[i], arguments)) {
+
+				case 0: //initialise variable
+				values[value_array] = value_array;
+				te_variable var = {.name = argv[i+1], 
+								.address = &(values[value_array]), 
+								.type = TE_VARIABLE, 
+								.context = NULL};
+				teVars[value_array] = var;
+				value_array++;
 				break;
 
 				case 1:
-				looping();
+				//looping();
 				break;
 
 				case 2:
-				sigfigures();
+				//sigfigures();
 				break;
 
 				default: break;
+			}
 		}
-	
+	}
+	else if (argc == 1) { //put this in the first if() statement?
+		// open file and do stuff make that a function?
+	}
+	else {			// if no arguments run normal calc 
+		printf("No loop variables were found\n");
+		scanf("%s", expression);
+    }
+
+	/*if (argv[argc-1]) {
+		char* inputfile = argv[argc-1];
+		FILE *file = fopen("%s", "r");
+
+		// do something with the file
+
 	}
 
-
+	fclose(file);
+	*/
     return 0;
 }

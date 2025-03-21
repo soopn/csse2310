0259bdb39c1611b7279b6d303bc4a14dbde72547
s4873930@uq.edu.au <s4873930@uq.edu.au@21130727-1c1a-8f4d-90a0-7afe4296f637>
2025-03-21 01:27:50 +0000

    added log comment section to main file to log bugs or notes, completed initialising of looping function in stdin, added more exit states regarding to incorrect stdin use
    git-svn-id: file:///mnt/c/Users/ivana/local-repo/trunk@5 21130727-1c1a-8f4d-90a0-7afe4296f637

--- a1/uqexpr.c
+++ a1/uqexpr.c
@@ -6,37 +6,12 @@
 #define TRUE 1
 #define FALSE 0
 
-/*
-bool var_check(char* input){
-    for (int i=0 ; i < strlen(input) ; i++){
-        if (input[i] != '=') {
-            return FALSE;
-        }
-        else {
-            return TRUE;
-        }
-    }
-}
-*/
+/* LOG */
+/* 
 
-/*
-bool eqn_check (char* input) {
-    for (int i=0 ; i<strlen(input) ; i++) {
-       if (input[i]  
-			   }
-*/
-/*
-int argument_check (char* input) {
-	if (input == "initialise") {
-		initialise();	// incomplete
-	}
-	else if (input == "sigfigures") {
-		sigfigures();	// incomplete
-	}
-	else if (input == "looping") {
-		looping();		// incomplete
-	}
-*/
+ * redundancy in exit(9) state, could make it all 1 big void retun checking function
+	
+																						*/
 
 int argument_check (int argc, char** input, char* arguments) {
 
@@ -61,9 +36,13 @@ void valid_use(int argc, char* argv[]) {
 
 int main (int argc, char* argv[]) //include some while clause for EOF e.g fgets()
 {
+
+    argc--; // argc starts at first argument not at ./uqexpr
 	char* arguments[3] = {"--initialise", "--sigfigures", "--looping"};
 	int init_count = 0;
+	int sigfigs = 0;
 
+	// for initialising variables
 	for (int i = 1 ; i < argc - 1; i++) {
 		if (strstr(argv[i], arguments[1])) {
 			init_count++;
@@ -72,8 +51,11 @@ int main (int argc, char* argv[]) //include some while clause for EOF e.g fgets(
 	te_variable teVars[init_count];	
 	double values[init_count];
 
-    argc--; // argc starts at first argument not at ./uqexpr
-    char* expression = malloc(sizeof(char**));
+	// for loop variables
+	char* loop_var[]; //smth wrong here
+
+	// command line argument stuff
+    char* expression = malloc(sizeof(char**)); 
 	if (argc > 1 ) { 
 		valid_use(argc, argv);
 		int value_array = 0;
@@ -91,34 +73,58 @@ int main (int argc, char* argv[]) //include some while clause for EOF e.g fgets(
 				break;
 
 				case 1:
-				//looping();
+				if (isalpha(argv[i+1][1] == 0)) {
+						fprintf(stderr, "uqexpr: invalid variable(s) were specified\n");
+						//free values
+						exit(6);
+						}
+				//store in array [var,start,inc,end]
+				char* temp = strtok(argv[i+1],",");
+				for (int j = 0 ; j < strlen(argv[i+1]) ; j++) {
+					strcat(loop_var,temp[j]);	//need to be stored as double at some point
+					temp = strtok(NULL,",");
+
+				}
+				if (strlen(loop_var) != 4) {
+					exit(); // find exit code
+				}
 				break;
 
 				case 2:
-				//sigfigures();
+				sigfigs = argv[i+1];	
+				if (sigfigs == 1) {
+					fprintf(stderr, "Usage: ./uqexpr [--looping string] [--sigfigures 2..9] [--initialise string] [filename]\n");
+					exit(9); 
+				}
 				break;
 
 				default: break;
 			}
 		}
-	}
+	} 
 	else if (argc == 1) { //put this in the first if() statement?
-		// open file and do stuff make that a function?
+		if (strstr(argv[1],"./")) {
+			char* inputfile = strtok(argv[1],"./");
+			FILE *file = fopen(inputfile,"r");
+			
+			//invalid file
+			if (file == NULL) {
+				fprintf(stderr,"uqexpr: can't open file %s for reading",argv[1]);
+				//free expressions
+				exit(9);
+			}
+			// run through main?
+		}
 	}
 	else {			// if no arguments run normal calc 
 		printf("No loop variables were found\n");
 		scanf("%s", expression);
+		if (expression){
+			printf("Result: %d\n", te_interp(expression),0);
+		}
     }
 
-	/*if (argv[argc-1]) {
-		char* inputfile = argv[argc-1];
-		FILE *file = fopen("%s", "r");
-
-		// do something with the file
-
-	}
 
 	fclose(file);
-	*/
     return 0;
 }

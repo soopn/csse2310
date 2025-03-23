1358bd268a265259aaa3092796444929ead9a7b1
s4873930@uq.edu.au <s4873930@uq.edu.au@21130727-1c1a-8f4d-90a0-7afe4296f637>
2025-03-23 11:04:53 +0000

    created 2 structs for loop variables and initialised variables, fixed bugs regarding wrong argument types in functions, implemented storing of variables in as te_variable type
    git-svn-id: file:///mnt/c/Users/ivana/local-repo/trunk@6 21130727-1c1a-8f4d-90a0-7afe4296f637

--- a1/uqexpr.c
+++ a1/uqexpr.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <tinyexpr.h>
 #include <stdlib.h>
+#include <ctype.h>
 #include <string.h>
 
 #define TRUE 1
@@ -10,10 +11,27 @@
 /* 
 
  * redundancy in exit(9) state, could make it all 1 big void retun checking function
-	
-																						*/
+ * needs duplicate variable check
+ 																					
+ 																						*/
+typedef struct {
+	char* name;
+	double value;
+} variable;
 
-int argument_check (int argc, char** input, char* arguments) {
+typedef struct {
+	char* name;
+	double start;
+	double inc;
+	double end;
+} loopvar;
+
+void printerr9() {
+	fprintf(stderr, "Usage: ./uqexpr [--looping string] [--sigfigures 2..9] [--initialise string] [filename]\n");
+	exit(9); 
+}
+
+int argument_check (int argc, char* input[], char* arguments[]) {
 
 	for (int i = 1 ; i < argc ; i++) {
 		for (int j = 0 ; j < 3 ; j++) {
@@ -25,74 +43,99 @@ int argument_check (int argc, char** input, char* arguments) {
 	return -1; // returning -1 will send it to default case
 }
 
-void valid_use(int argc, char* argv[]) { 
+void valid_use (int argc, char* argv[]) { 
 	for (int i = 1 ; i < argc - 1 ; i+=2) {  // skip over any strings that would follow the arguments
 		if (strstr(argv[i],"--") == FALSE) { // if any file before the arguments exit program
-		fprintf(stderr, "Usage: ./uqexpr [--looping string] [--sigfigures 2..9] [--initialise string] [filename]\n");
-		exit(9); 
+		printerr9();
 		}
 	}
 }
 
+void valid_var (char* string, variable x) { //incomplete
+	int equal_count = 0;
+	for (int i = 0 ; i < (int)(strlen(string)) ; i++) {
+
+		if (string[i] == "=") {
+			equal_count++;
+		}
+	}
+	if (equal_count != 1) {
+		fprintf(stderr, "uqexpr: invalid variable(s) were specified");
+		exit(6);
+	}
+}
+
+/*
+ 
+int valid_expr(char* string, te_variable vars[]) {
+	int errPos;
+	te_expr* expr = te_compile; // incomplete
+}
+													*/
 int main (int argc, char* argv[]) //include some while clause for EOF e.g fgets()
 {
 
     argc--; // argc starts at first argument not at ./uqexpr
-	char* arguments[3] = {"--initialise", "--sigfigures", "--looping"};
+	char* arguments[3] = {"--initialise", "--looping", "--sigfigures"};
 	int init_count = 0;
-	int sigfigs = 0;
+	char sigfigs = 0;
 
-	// for initialising variables
+	// number of variables to initialise
 	for (int i = 1 ; i < argc - 1; i++) {
 		if (strstr(argv[i], arguments[1])) {
 			init_count++;
 		}
 	}
-	te_variable teVars[init_count];	
-	double values[init_count];
+	variable *vars = malloc(init_count * sizeof(variable));
+	te_variable *te_vars = malloc(init_count * sizeof(te_variable));
+
+//---------------------------------------------------------------------------------------------------------//
 
 	// for loop variables
-	char* loop_var[]; //smth wrong here
+	loopvar *loops = malloc(sizeof(loopvar));
 
 	// command line argument stuff
-    char* expression = malloc(sizeof(char**)); 
+    char* expression = malloc(sizeof(char*)); 	
 	if (argc > 1 ) { 
 		valid_use(argc, argv);
 		int value_array = 0;
+		int loop_array = 0;
 		for (int i = 1 ; i < argc - 1 ; i++) {		// find which arguments to run (-1 to exclude filename)
-			switch(argument_check(argc, argv[i], arguments)) {
+			
+			// needs a valid use check e.g blank argument after --initialise, should exit 9, print to stderr
 
-				case 0: //initialise variable
-				values[value_array] = value_array;
-				te_variable var = {.name = argv[i+1], 
-								.address = &(values[value_array]), 
-								.type = TE_VARIABLE, 
-								.context = NULL};
-				teVars[value_array] = var;
+			switch(argument_check(argc, argv[i], arguments)) { // 0 = initialise ; 1 = looping ; 2 = sigfigures
+
+				case 0: //initialise variable (declare them in the variable struct first then map them to a te_variable later	
+				vars[value_array].name = strdup(strtok(argv[i+1],"="));
+				vars[value_array].value = strdup(strtok(NULL,"=")); 
 				value_array++;
 				break;
-
-				case 1:
+//-----------------------------------------------------------------------------------------------------------------//
+				case 1: //looping values
 				if (isalpha(argv[i+1][1] == 0)) {
 						fprintf(stderr, "uqexpr: invalid variable(s) were specified\n");
 						//free values
 						exit(6);
-						}
+				}
 				//store in array [var,start,inc,end]
-				char* temp = strtok(argv[i+1],",");
-				for (int j = 0 ; j < strlen(argv[i+1]) ; j++) {
-					strcat(loop_var,temp[j]);	//need to be stored as double at some point
-					temp = strtok(NULL,",");
+								
+				if (strlen((char*)(loop_var)) != 4) {
+					fprintf(stderr, "uqexpr: invalid variable(s) were specified\n");	
+					exit(6); 
+				}
+				loops[loop_array] = {.name = strdup(strtok(argv[i+1],","),
+									 .start = strdup(strtok(NULL,",")),
+									 .inc = strdup(strtok(NULL,",")),
+									 .end = strdup(strtok(NULL,","))
+									};
+				loop_array++;	
 
-				}
-				if (strlen(loop_var) != 4) {
-					exit(); // find exit code
-				}
 				break;
-
-				case 2:
-				sigfigs = argv[i+1];	
-				if (sigfigs == 1) {
+//-----------------------------------------------------------------------------------------------------------------//
+				case 2: //sig figures
+				sigfigs = argv[i+1][1];	
+				if (sigfigs == 1 || isdigit(argv[i+1]) == 0) {
 					fprintf(stderr, "Usage: ./uqexpr [--looping string] [--sigfigures 2..9] [--initialise string] [filename]\n");
 					exit(9); 
 				}
@@ -101,7 +144,14 @@ int main (int argc, char* argv[]) //include some while clause for EOF e.g fgets(
 				default: break;
 			}
 		}
-	} 
+		//mapping variable to te_variable
+		for (int i = 0 ; i < init_count ; i++) {
+			te_variable var = {.name = vars[i].name,
+							   .address = &(vars[i].value),
+							   .type = TE_VARIABLE,
+							   .context = NULL};
+		} 
+
 	else if (argc == 1) { //put this in the first if() statement?
 		if (strstr(argv[1],"./")) {
 			char* inputfile = strtok(argv[1],"./");
@@ -118,13 +168,18 @@ int main (int argc, char* argv[]) //include some while clause for EOF e.g fgets(
 	}
 	else {			// if no arguments run normal calc 
 		printf("No loop variables were found\n");
-		scanf("%s", expression);
-		if (expression){
-			printf("Result: %d\n", te_interp(expression),0);
+		while (!feof(stdin)) {
+			scanf("%s", expression); //might be better to use fgets()
+			if (expression){
+				printf("Result: %d\n", te_interp(expression,0));
+			}
 		}
+		free(expression);
+		fprintf(stdout, "Thank you for using uqexpr!");
+		exit(0);
     }
 
-
+//te_free()
 	fclose(file);
     return 0;
 }

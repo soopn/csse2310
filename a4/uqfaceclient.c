ecf75e13eb980d3823deb6820d174fd4d94c0c6c
s4873930@uq.edu.au <s4873930@uq.edu.au@21130727-1c1a-8f4d-90a0-7afe4296f637>
2025-05-19 09:29:02 +0000

    written basic makefile for both client and server, added command line parsing capabilities to client
    git-svn-id: file:///mnt/c/Users/ivana/local-repo/trunk@80 21130727-1c1a-8f4d-90a0-7afe4296f637

---
+++ a4/uqfaceclient.c
@@ -0,0 +1,151 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+// CONST STRINGS
+const char* const usageErrMsg
+        = "Usage: ./uqfaceclient portnum [--replacefilename filename] "
+          "[--outputfilename filename] [--detectimage filename]\n";
+const char* const fileReadErr = "uqfaceclient: unable to open the input file \"%s\" for reading\n";
+const char* const fileWriteErr = "uqfaceclient: cannot open the output file \"%s\" for writing\n";
+const char* const portErrMsg = "uqfaceclient: unable to connect to the server on port \"%d\"\n";
+
+// CONST CMD LINE ARGUMENTS
+const char* const replaceFileArg = "--replacefilename";
+const char* const outputFileArg = "--outputfilename";
+const char* const outputFileVariation = ">";
+const char* const detectImgArg = "--detectimage";
+const char* const detectImgVariation = "<";
+
+typedef enum {
+	EXIT_USAGE = 8,
+	EXIT_FILE_READ = 19,
+	EXIT_FILE_WRITE = 18,
+	EXIT_PORT = 2 
+} ExitStatus;
+
+typedef struct {
+	char* replaceFileName;
+	FILE* replaceFile;
+	char* outputFileName;
+	FILE* outputFile;
+	char* imgFileName;
+	FILE* imgFile;
+} Arguments;
+
+void usage_error();
+bool is_argument (char* string);
+void duplicate_argument_check (Arguments* args, const char* const argument);
+Arguments* init_arguments ();
+Arguments* parse_command_line (int argc, char** argv);
+
+/* COMMAND LINE ARGUMENTS
+ * USAGE: ./uqfaceclient portnum [--replacefilename filename] [--outputfilename filename] [--detectimage filename]
+ * portnum must always be the first argument cannot be empty
+ */
+int main(int argc, char** argv)
+{
+	Arguments* programArgs = parse_command_line(argc, argv);
+    return 0;
+}
+
+void has_empty_string (int argc, char** argv) {
+	argv++; // remove program name
+	argc--;
+	for (int i = 0; i < argc ; i++) {
+		if (!strcmp(argv[i], "")) {
+			usage_error();
+		}
+	}
+	return;
+}
+
+// increment the iteration count and checks if the current iteration exceeds total arguments
+int increment_and_check (int iteration, int argc) {
+	iteration++;
+	if (iteration >= argc) {
+		usage_error();
+	}
+	return iteration;
+}
+
+bool is_argument (char* string) {
+	if (strcmp(string, replaceFileArg)) {
+		return false;
+	}
+	if (strcmp(string, outputFileArg) || strcmp(string, outputFileVariation)) {
+		return false;
+	}
+	if (strcmp(string, detectImgArg) || strcmp(string, detectImgVariation)) {
+		return false;
+	}
+	return true;
+}
+ 
+void usage_error() {
+	fprintf(stderr, usageErrMsg);
+	exit(EXIT_USAGE);
+}
+
+// check if given argument has already been initialised in args
+void duplicate_argument_check (Arguments* args, const char* const argument) {
+	if (argument == replaceFileArg) {
+		if (args->replaceFileName != NULL) {
+			usage_error();
+		}
+	}
+	if (argument == outputFileArg) {
+		if (args->outputFileName != NULL) {
+			usage_error();
+		}
+	}
+	if (argument == detectImgArg) {
+		if (args->imgFileName != NULL) {
+			usage_error();
+		}
+	}
+}
+
+Arguments* init_arguments () { // sets all pointers to NULL
+	Arguments* args = (Arguments*)malloc(sizeof(Arguments));
+	args->replaceFileName = NULL;
+	args->replaceFile = NULL;
+	args->outputFileName = NULL;
+	args->outputFile = NULL;
+	args->imgFileName = NULL;
+	args->imgFile = NULL;
+	return args;
+}
+
+Arguments* parse_command_line (int argc, char** argv) {
+	has_empty_string(argc, argv); // check for empty string
+	argv += 2; // remove program name and portnum
+	argc -= 2;
+	Arguments* args = init_arguments(); // malloc'd
+
+	for (int i = 0 ; i < argc; i++) {
+		if (!strcmp(argv[i], replaceFileArg)) {
+			i = increment_and_check(i, argc);
+			if (!is_argument(argv[i])) {
+				duplicate_argument_check(args, replaceFileArg);
+				args->replaceFileName = strdup(argv[i]);
+			}
+		}
+		if (!strcmp(argv[i], outputFileArg) || !strcmp(argv[i], outputFileVariation)) {
+			i = increment_and_check(i, argc);
+			if (!is_argument(argv[i])) {
+				duplicate_argument_check(args, outputFileArg);
+				args->outputFileName = strdup(argv[i]);
+			}
+		}
+		if (!strcmp(argv[i], detectImgArg) || !strcmp(argv[i], detectImgVariation)) {
+			i = increment_and_check(i, argc);
+			if (!is_argument(argv[i])) {
+				duplicate_argument_check(args, detectImgArg);
+				args->imgFileName = strdup(argv[i]);
+			}
+		}
+	}
+	return args;
+}
